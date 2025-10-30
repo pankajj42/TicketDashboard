d28d967b0bb0be3c0ed34a040e7ae2f856d41bbe
pankajj42 <pankajjangid42@gmail.com>
2025-10-30 21:03:03 +0530

    Working Frontend boiler plate code - added tailwindcss + shadcn ui

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+	return (
+		<div className="flex min-h-svh flex-col items-center justify-center">
+			<h1 className="mb-4 text-2xl font-bold">Welcome to TicketDash!</h1>
+			<Button>Click me</Button>
+		</div>
+	);
+}
+
+export default App;
